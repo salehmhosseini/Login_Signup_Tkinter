ae48c05406c75cc398af3954ba0b511f7b2724d9
salehmhosseini <97988967+salehmhosseini@users.noreply.github.com>
2022-07-24 19:47:13 +0430

    forget password page uploaded

---
+++ src/ForgetPasswordPage.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+
+class forgetPassword:
+    def __init__(self):
+        root = Tk()
+        root.geometry("1000x650")
+        root.title("Login Page")
+        root.resizable(False, False)
+        root.mainloop()
